e72faa8fcb0430245558c995edad84b7eb9635d4
Jan Kiesewetter <jan@t3easy.de>
2024-03-11 12:26:59 +0100

    Use UMD template
    See: https://github.com/umdjs/umd/blob/master/templates/jqueryPlugin.js

--- js/ion-tabs/ion.tabs.js
+++ js/ion-tabs/ion.tabs.js
@@ -9,8 +9,35 @@
 // http://ionden.com/a/plugins/licence-en.html
 // =====================================================================================================================
 
-(function ($, document, window, location) {
+// Uses CommonJS, AMD or browser globals to create a jQuery plugin.
 
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['jquery'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node/CommonJS
+        module.exports = function( root, jQuery ) {
+            if ( jQuery === undefined ) {
+                // require('jQuery') returns a factory that requires window to
+                // build a jQuery instance, we normalize how we use modules
+                // that require this pattern but the window provided is a noop
+                // if it's defined (how jquery works)
+                if ( typeof window !== 'undefined' ) {
+                    jQuery = require('jquery');
+                }
+                else {
+                    jQuery = require('jquery')(root);
+                }
+            }
+            factory(jQuery);
+            return jQuery;
+        };
+    } else {
+        // Browser globals
+        factory(jQuery);
+    }
+}(function ($) {
     if ($.ionTabs) {
         return;
     }
@@ -211,7 +238,7 @@
 
 
     // Plugin methods
-    $.ionTabs = function (selector, options) {
+    $.fn.ionTabs = function (selector, options) {
         if (!selector) {
             return;
         }
@@ -236,8 +263,8 @@
 
 
     // Plugin Public methods
-    $.ionTabs.setTab = function (group, name) {
+    $.fn.ionTabs.setTab = function (group, name) {
         tabs[group].setTab(name);
     };
 
-}(jQuery, document, window, location));
+}));
