f4db00df3e41e44ee3ca194dc6f7911b70ff88a2
Jan Kiesewetter <jan@t3easy.de>
2024-03-12 13:21:58 +0100

    Update UMD header

--- js/ion-tabs/ion.tabs.js
+++ js/ion-tabs/ion.tabs.js
@@ -11,33 +11,18 @@
 
 // Uses CommonJS, AMD or browser globals to create a jQuery plugin.
 
-(function (factory) {
+;(function(factory) {
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
         define(['jquery'], factory);
-    } else if (typeof module === 'object' && module.exports) {
+    } else if (typeof exports === 'object') {
         // Node/CommonJS
-        module.exports = function( root, jQuery ) {
-            if ( jQuery === undefined ) {
-                // require('jQuery') returns a factory that requires window to
-                // build a jQuery instance, we normalize how we use modules
-                // that require this pattern but the window provided is a noop
-                // if it's defined (how jquery works)
-                if ( typeof window !== 'undefined' ) {
-                    jQuery = require('jquery');
-                }
-                else {
-                    jQuery = require('jquery')(root);
-                }
-            }
-            factory(jQuery);
-            return jQuery;
-        };
+        factory(require('jquery'));
     } else {
         // Browser globals
         factory(jQuery);
     }
-}(function ($) {
+}(function($) {
     if ($.ionTabs) {
         return;
     }
@@ -238,7 +223,7 @@
 
 
     // Plugin methods
-    $.fn.ionTabs = function (selector, options) {
+    $.ionTabs = function (selector, options) {
         if (!selector) {
             return;
         }
@@ -263,7 +248,7 @@
 
 
     // Plugin Public methods
-    $.fn.ionTabs.setTab = function (group, name) {
+    $.ionTabs.setTab = function (group, name) {
         tabs[group].setTab(name);
     };
 
